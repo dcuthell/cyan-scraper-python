9c4227e2603a224fa53d50ca03342aaddb7ed3c0
David Cuthell <dcc1447@gmail.com>
2018-09-17 23:02:41 -0700

    make a crappy refactor

--- cyan-scraper.py
+++ cyan-scraper.py
@@ -3,21 +3,32 @@ from requests import get
 import re
 import locale
 
+def parseRent(rentstring):
+    rentLow = rentstring[0].get_text().split('-', 1)[0]
+    if(rentLow == 'Call'):
+        return 0;
+    rent = int(re.sub(r'[^0-9'+decimal+r']+','',rentLow))
+    return rent;
+
+def parseSqft(aptUnit):
+    return int(aptUnit.find_all('td', attrs={"data-label": "Sq. Ft."})[0].get_text());
+
+def parseAptNum(aptUnit):
+    return aptUnit.find_all('td', attrs={"data-label": "Apartment"})[0].get_text();
+
 with get('https://www.cyanpdx.com/apartmentsearchresult.aspx?Bed=-1&rent=&MoveInDate=&myOlePropertyId=207912&UnitCode=&control=1') as response:
-    listings = [{}]
     webpage = response.text
     decimal=locale.localeconv()['decimal_point']
     soup = BeautifulSoup(webpage,'html.parser')
-    for aptUnit in soup.find_all('tr'):
-        rentOutput = aptUnit.find_all('td', attrs={"data-label": "Rent"})
-        if(rentOutput == []):
+    for aptunit in soup.find_all('tr'):
+        rentstring = aptunit.find_all('td', attrs={"data-label": "Rent"})
+        if(rentstring == []):
             continue;
-        rentset = rentOutput[0].get_text().split('-', 1)[0]
-        if(rentset == 'Call'):
+        rent = parseRent(rentstring)
+        if(rent == 0):
             continue;
-        rent = int(re.sub(r'[^0-9'+decimal+r']+','',rentset))
-        aptUnitNum = aptUnit.find_all('td', attrs={"data-label": "Apartment"})[0].get_text()
-        sqft = int(aptUnit.find_all('td', attrs={"data-label": "Sq. Ft."})[0].get_text())
+        aptUnitNum = parseAptNum(aptunit)
+        sqft = parseSqft(aptunit)
         apartment = "Apartment: " + str(aptUnitNum) + " for: $" + str(rent) + " with: " + str(sqft) + "sqft"
         if(rent/sqft < 2.19):
             print(apartment)
