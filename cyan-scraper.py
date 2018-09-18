5123277616df6cf2794ad1bf67588669d0ebb37c
David Cuthell <dcc1447@gmail.com>
2018-09-18 11:09:50 -0700

    make a better code refactor

--- cyan-scraper.py
+++ cyan-scraper.py
@@ -3,7 +3,8 @@ from requests import get
 import re
 import locale
 
-def parseRent(rentstring):
+def parseRent(aptunit):
+    rentstring = aptunit.find_all('td', attrs={"data-label": "Rent"})
     rentLow = rentstring[0].get_text().split('-', 1)[0]
     if(rentLow == 'Call'):
         return 0;
@@ -20,11 +21,9 @@ with get('https://www.cyanpdx.com/apartmentsearchresult.aspx?Bed=-1&rent=&MoveIn
     webpage = response.text
     decimal=locale.localeconv()['decimal_point']
     soup = BeautifulSoup(webpage,'html.parser')
-    for aptunit in soup.find_all('tr'):
-        rentstring = aptunit.find_all('td', attrs={"data-label": "Rent"})
-        if(rentstring == []):
-            continue;
-        rent = parseRent(rentstring)
+    table = soup.find_all('tr', class_='AvailUnitRow')
+    for aptunit in table:
+        rent = parseRent(aptunit)
         if(rent == 0):
             continue;
         aptUnitNum = parseAptNum(aptunit)
