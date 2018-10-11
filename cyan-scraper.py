cf2901b9c9d00d4d95458ab2c01301be37983472
David Cuthell <dcc1447@gmail.com>
2018-10-11 09:44:41 -0700

    print dataset for ladd

--- cyan-scraper.py
+++ cyan-scraper.py
@@ -16,19 +16,40 @@ def parseSqft(aptUnit):
 def parseAptNum(aptUnit):
     return aptUnit.find_all('td', attrs={"data-label": "Apartment"})[0].get_text();
 
-with get('https://www.cyanpdx.com/apartmentsearchresult.aspx?Bed=-1&rent=&MoveInDate=&myOlePropertyId=207912&UnitCode=&control=1') as response:
+# with get('https://www.cyanpdx.com/apartmentsearchresult.aspx?Bed=-1&rent=&MoveInDate=&myOlePropertyId=207912&UnitCode=&control=1') as response:
+#     webpage = response.text
+#     decimal=locale.localeconv()['decimal_point']
+#     soup = BeautifulSoup(webpage,'html.parser')
+#     for aptunit in soup.find_all('tr'):
+#         rentstring = aptunit.find_all('td', attrs={"data-label": "Rent"})
+#         if(rentstring == []):
+#             continue;
+#         rent = parseRent(rentstring)
+#         if(rent == 0):
+#             continue;
+#         aptUnitNum = parseAptNum(aptunit)
+#         sqft = parseSqft(aptunit)
+#         apartment = "Apartment: " + str(aptUnitNum) + " for: $" + str(rent) + " with: " + str(sqft) + "sqft"
+#         if(rent/sqft < 2.19):
+#             print(apartment)
+
+with get('https://www.hollandresidential.com/ladd/floor-plans/') as response:
     webpage = response.text
     decimal=locale.localeconv()['decimal_point']
     soup = BeautifulSoup(webpage,'html.parser')
-    for aptunit in soup.find_all('tr'):
-        rentstring = aptunit.find_all('td', attrs={"data-label": "Rent"})
-        if(rentstring == []):
-            continue;
-        rent = parseRent(rentstring)
-        if(rent == 0):
-            continue;
-        aptUnitNum = parseAptNum(aptunit)
-        sqft = parseSqft(aptunit)
-        apartment = "Apartment: " + str(aptUnitNum) + " for: $" + str(rent) + " with: " + str(sqft) + "sqft"
-        if(rent/sqft < 2.19):
-            print(apartment)
+    # print(soup)
+    scripts = soup.find_all("script")
+    print(scripts[len(scripts)-2])
+    # for aptunit in soup.find_all('tr'):
+    #     print(aptunit)
+        # rentstring = aptunit.find_all('td', attrs={"data-label": "Rent"})
+        # if(rentstring == []):
+        #     continue;
+        # rent = parseRent(rentstring)
+        # if(rent == 0):
+        #     continue;
+        # aptUnitNum = parseAptNum(aptunit)
+        # sqft = parseSqft(aptunit)
+        # apartment = "Apartment: " + str(aptUnitNum) + " for: $" + str(rent) + " with: " + str(sqft) + "sqft"
+        # if(rent/sqft < 2.19):
+        #     print(apartment)
